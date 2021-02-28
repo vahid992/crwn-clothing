f9db705946cd031e8403e682709f990656df70fb
vahid992 <vnmarch92@gmail.com>
2021-02-28 13:59:39 +0330

    update our menu-items to navigate withRouter HOV

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import HomePage from './pages/homeage/homepage.component';
+import {Route,Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
