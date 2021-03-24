a78dccd4761f34a3e17bb3829e4a7f5b6a700dbe
vahid992 <vnmarch92@gmail.com>
2021-03-24 12:44:10 +0430

    sign in and sign out header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Component} from 'react';
 import HomePage from './pages/homeage/homepage.component';
 import ShopPage from './pages/shop-page/shop-page.component';
 import {Route,Switch} from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import Header from './comonents/header/header.component';
 import  SingInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 class App extends Component  {
@@ -17,9 +17,25 @@ class App extends Component  {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user})
-    })
+    debugger;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      this.setState({currentUser:userAuth})
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          });   
+          console.log(this.state); 
+        });
+        
+      }
+      this.setState({currentUser:userAuth});
+    });
   }
 
   componentWillUnmount(){
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,8 +1,12 @@
-import './sign-in-and-sign-up.styles.scss';
-import SignIn from '../../comonents/sign-in/sign-in.component';
+import "./sign-in-and-sign-up.styles.scss";
+import SignIn from "../../comonents/sign-in/sign-in.component";
+import SignUp from "../../comonents/sign-up/sign-up.component";
 
-const SingInAndSignUpPage = ()=>(
-    <div className='sign-in-and-sign-up'><SignIn /></div>
-)
+const SingInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default SingInAndSignUpPage;
